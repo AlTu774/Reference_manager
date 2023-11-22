3add3ee91b2ba77bc457ecafde7d7dac3ed82815
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2023-11-22 11:53:37 +0200

    Add from functionality
    Co-authored-by: ElliJohansson <elli.johansson@live.com>

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
 from flask import redirect, render_template, request
 from app import app
+from repositories import books_repository
 
 @app.route("/")
 def index():
@@ -11,6 +12,13 @@ def add():
         return render_template("add.html")
     
     if request.method == "POST":
+        tag = request.form["tag"]
+        title = request.form["title"]
+        author = request.form["author"]
+        publish_year = request.form["publish_year"]
+        publisher = request.form["publisher"]
+        books_repository.insert_book(tag, title, author, publish_year, publisher)
+
         return render_template("add.html")
 
 @app.route("/list", methods=["GET"])
