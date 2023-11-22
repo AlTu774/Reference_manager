be360bd9b7cead62f5376d17ff9847300928b0b0
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2023-11-22 11:44:35 +0200

    add insert book method to service
    Co-authored-by: ElliJohansson <elli.johansson@live.com>

--- src/repositories/books_repository.py
+++ src/repositories/books_repository.py
@@ -1,9 +1,9 @@
 from sqlalchemy import text
 from db import db
 
-def insert_book(tag, title, author, publish_year, publisher):
+def insert_book(book):
     sql = """INSERT INTO books (tag, title, author, publish_year, publisher)
         VALUES (:tag, :title, :author, :publish_year, :publisher)"""
-    db.session.execute(text(sql), {"tag":tag, "title":title, "author":author,
-                                   "publish_year":publish_year, "publisher":publisher})
+    db.session.execute(text(sql), {"tag":book.tag, "title":book.title, "author":book.author,
+                                   "publish_year":book.publish_year, "publisher":book.publisher})
     db.session.commit()
---
+++ src/services/source_service.py
@@ -0,0 +1,11 @@
+from entities.source import Source
+
+def insert_book(tag, title, author, publish_year, publisher, service):
+    new_book = Source(
+        tag,
+        title,
+        author,
+        publish_year,
+        publisher
+    )
+    service.insert_book(new_book)
