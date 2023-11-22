da80761508c6c1f710de5a5ff0ab94220dbb391c
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2023-11-22 11:06:12 +0200

    Add source object

---
+++ src/entities/source.py
@@ -0,0 +1,7 @@
+class Source:
+    def __init__(self, tag, title, author, publish_year, publisher):
+        self.tag = tag
+        self.title = title
+        self.author = author
+        self.publish_year = int(publish_year)
+        self.publisher = publisher
