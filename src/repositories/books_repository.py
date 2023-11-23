21620cea852a30618f2963546462ea54163890f3
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-11-23 15:05:24 +0200

    feat(repo): add SQLcmd to get all books to test insert_book

--- src/repositories/books_repository.py
+++ src/repositories/books_repository.py
@@ -7,3 +7,7 @@ def insert_book(book):
     db.session.execute(text(sql), {"tag":book.tag, "title":book.title, "author":book.author,
                                    "publish_year":book.publish_year, "publisher":book.publisher})
     db.session.commit()
+
+def get_books():
+    sql = """ SELECT id, tag, title, author, publish_year, publisher FROM books"""
+    return db.session.execute(text(sql)).fetchall()
