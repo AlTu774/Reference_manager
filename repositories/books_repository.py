995fa084b8623a731362b4b5b0983bcb96876bd6
mseppi <miikka.seppi123@gmail.com>
2023-12-01 01:24:40 +0200

    fix code for pylint

--- repositories/books_repository.py
+++ repositories/books_repository.py
@@ -4,8 +4,11 @@ from db import db
 def insert_book(book):
     sql = """INSERT INTO books (tag, title, author, publish_year, publisher)
         VALUES (:tag, :title, :author, :publish_year, :publisher)"""
-    db.session.execute(text(sql), {"tag":book.tag, "title":book.title, "author":book.author,
-                                   "publish_year":book.publish_year, "publisher":book.publisher})
+    db.session.execute(text(sql), {"tag":book.tag,
+                                    "title":book.title,
+                                    "author":book.author,
+                                   "publish_year":book.publish_year,
+                                     "publisher":book.publisher})
     db.session.commit()
 
 def get_books():
