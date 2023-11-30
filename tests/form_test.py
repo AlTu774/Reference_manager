f3aceef40ecc4baca933a1b35ad4f5bf77568e3f
ElliJohansson <elli.johansson@live.com>
2023-11-30 21:14:02 +0200

    Add initial form test with valid inputs

---
+++ tests/form_test.py
@@ -0,0 +1,18 @@
+import unittest
+from app import app
+
+class TestReferenceForm(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_form_submission_with_valid_inputs(self):
+        response = self.app.post("/add", data=dict(
+            tag="some tag",
+            title="some title",
+            author="some author",
+            publish_year=2000,
+            publisher="some publiser"
+
+        ))
+
+        self.assertEqual(response.status_code, 200)
