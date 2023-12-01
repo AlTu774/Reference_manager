7a223a5436349162197c471532e7194686381fa4
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-12-01 11:02:28 +0200

    fix(db): revert conditional DATABASE_URL variable

--- db.py
+++ db.py
@@ -7,9 +7,8 @@ MODE = environ.get("MODE") or "production"
 DATABASE_URL = environ.get("DATABASE_URL")
 if MODE == "test":
     DATABASE_URL = environ.get("TEST_DATABASE_URL")
-if DATABASE_URL:
-    DATABASE_URL = DATABASE_URL.replace('://', 'ql://', 1) \
-        if DATABASE_URL.startswith('postgres://') else DATABASE_URL
+DATABASE_URL = DATABASE_URL.replace('://', 'ql://', 1) \
+    if DATABASE_URL.startswith('postgres://') else DATABASE_URL
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
 
