c8633b6cf5b1ef936f19108aa0017384ec98a6c6
Yuusuf <127731397+Yusuboy@users.noreply.github.com>
2023-12-11 21:22:28 +0200

    Update users_repository.py
    Muutettiin SQL-lausekkeiden ja taulujen nimiä nimetyiksi vakioiksi. Tämä tekee koodista luettavampaa ja ylläpidettävämpää.

--- repositories/users_repository.py
+++ repositories/users_repository.py
@@ -4,33 +4,37 @@ from db import db
 
 
 
+USERS_TABLE = "Users"
+USERNAME_COL = "username"
+PASSWORD_COL = "password"
+
 def register(username, password):
     hash_value = generate_password_hash(password)
-    sql = """INSERT INTO Users (username, password)
-    VALUES (:username, :password)"""
-    db.session.execute(text(sql), {"username":username, "password":hash_value})
+    sql = f"""INSERT INTO {USERS_TABLE} ({USERNAME_COL}, {PASSWORD_COL})
+              VALUES (:username, :password)"""
+    db.session.execute(text(sql), {"username": username, "password": hash_value})
     db.session.commit()
 
 def login(username, password):
-    sql = "SELECT id, password FROM Users WHERE username=:username"
-    result = db.session.execute(text(sql), {"username":username})
+    sql = f"SELECT id, {PASSWORD_COL} FROM {USERS_TABLE} WHERE {USERNAME_COL}=:username"
+    result = db.session.execute(text(sql), {"username": username})
     user = result.fetchone()
 
     if not user:
         return "Invalid username"
 
-    hash_value = user.password
+    hash_value = user[1]
 
     if not check_password_hash(hash_value, password):
         return "Invalid password"
     return True
 
 def get_all_usernames():
-    sql = "SELECT username FROM Users"
+    sql = f"SELECT {USERNAME_COL} FROM {USERS_TABLE}"
     result = db.session.execute(text(sql))
     return [row[0] for row in result.fetchall()]
 
 def delete_all_users():
-    sql = "DELETE FROM Users"
+    sql = f"DELETE FROM {USERS_TABLE}"
     db.session.execute(text(sql))
     db.session.commit()
