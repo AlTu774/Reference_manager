cd8648d8dccbab37b70e21ef6f01e6990a0c5c53
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2023-12-13 12:13:27 +0200

    Shorten a line

--- routes.py
+++ routes.py
@@ -73,7 +73,8 @@ def reset_users():
 
 @app.route("/bibtex", methods=["GET"])
 def create_bibtex_file(service = bibtex_service):
-    service.create_bibtex_file("references", source_service, books_repository, session["user_id"])
+    service.create_bibtex_file("references", source_service, books_repository,
+                               session["user_id"])
     return render_template("bibtex.html")
 
 @app.route("/download", methods=["GET"])
