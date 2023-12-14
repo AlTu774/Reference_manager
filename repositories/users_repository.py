b471383af0c5341bec3423eba76320a810a8a1db
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-12-14 22:34:22 +0200

    style(userrepo): remove extra linebreaks

--- repositories/users_repository.py
+++ repositories/users_repository.py
@@ -2,8 +2,6 @@ from sqlalchemy import text
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 
-
-
 def register(username, password):
     hash_value = generate_password_hash(password)
     sql = """INSERT INTO Users (username, password)
